8b64e85060c6918fd0ab8cba53a5dad4ae4b1992
hzhiqin <huang_zq@yahoo.com>
2018-12-23 16:49:33 +0800

    Add files via upload

--- get_prime_factor.py
+++ get_prime_factor.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Dec 23 15:52:08 2018
-@author: huang
-"""
 
 from multiprocessing import Pool
 import random
@@ -24,7 +19,7 @@ def prime_factor(value):
 
 if __name__ == "__main__":
     pool = Pool()
-    to_factor = [random.randint(100000, 5000000) for i in range(20)]
+    to_factor = [random.randint(100000, 5000000) for i in range(100)]
     results = pool.map(prime_factor, to_factor)
     for value, factor in zip(to_factor, results):
         print("The factors of {} are {}".format(value, factor))
